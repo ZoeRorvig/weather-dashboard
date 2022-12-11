72f150b58b99468f17d04711a72294e54f31d7b8
Zoe Rorvig <zrorvig@gmail.com>
2022-12-10 22:51:38 -0500

    added city buttons

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,34 @@ var apiKey = "ce95c0b37caa62e1aa9677a221191ebc";
 
 var searchBtnEl = document.querySelector("#search-button");
 var cityInputEl = document.querySelector("#cityInput");
+var savedCitiesEl = document.querySelector("#saved-cities");
 
 var cities = [];
+var savedCities = [];
+var btn;
 
 searchBtnEl.addEventListener("click", function(event){
-    city = cityInputEl.value;
-    cities.push(city);
+    var cityInput = cityInputEl.value;
+    cities.push(cityInput);
     localStorage.setItem("cities",JSON.stringify(cities));
+    displayCity();
 });
+
+var displayCity = function(){
+    savedCities = JSON.parse(localStorage.getItem("cities"));
+
+    savedCitiesEl.innerHTML = null;
+
+    for (var i = 0; i < cities.length; i++) {
+        var city = cities[i];
+    
+        btn = document.createElement("button");
+        btn.textContent = city;
+        btn.setAttribute("data-index", i);
+        btn.setAttribute("id", "cityBtn");
+        btn.type = "button";
+        btn.name = "cityBtn";
+        savedCitiesEl.appendChild(btn);
+      };
+      return;
+};
