54d34272b6d514a3b9930bbcb9c97bb7cd55db94
Zoe Rorvig <zrorvig@gmail.com>
2022-12-10 22:43:12 -0500

    saving cities to localStorage

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,12 @@
 var apiKey = "ce95c0b37caa62e1aa9677a221191ebc";
 
 var searchBtnEl = document.querySelector("#search-button");
+var cityInputEl = document.querySelector("#cityInput");
+
+var cities = [];
 
 searchBtnEl.addEventListener("click", function(event){
-    
+    city = cityInputEl.value;
+    cities.push(city);
+    localStorage.setItem("cities",JSON.stringify(cities));
 });
